272a73e6c626aa12e41f2703fb669c628f87fb9e
Dane Matrix <danema7rix@gmail.com>
2020-01-03 17:01:39 -0500

    missing files readded

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,41 +1,15 @@
 import React from 'react';
+
+import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 
-import Auxiliary from '../hoc/Auxiliary';
-
-// const layout = (props) => (
-// 	<Auxiliary>
-// 		<div>ToolBar, Sidedrawer, Backdrop</div>
-
-// 		<main>{props.children}</main>
-// 	</Auxiliary>
-// );
-
-// const layout2 = (props) => (
-// 	<Auxiliary>
-// 		<div>Toolbar, Sidedrawrer, Backdrop</div>
-// 		<main>{props.children}</main>
-// 	</Auxiliary>
-// );
-
-// const layout3 = (props) => (
-// 	<Auxiliary>
-// 		<div>Toolbar, sidedrawer, backdrop</div>
-// 		<main>{props.children}</main>
-// 	</Auxiliary>
-// );
-
-// const layout5 = (props) => (
-// 	<Auxiliary>
-// 		<div>toolbar, sidebar, backdrop</div>
-// 		<main>{props.children}</main>
-// 	</Auxiliary>
-// );
-
-const layout = (props) => (
-	<Auxiliary>
-		<div>Toolbar, Sidedrawer, Backdrop</div>
-		<main className={classes.Content}>{props.children}</main>
-	</Auxiliary>
+const layout = ( props ) => (
+    <Aux>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main className={classes.Content}>
+            {props.children}
+        </main>
+    </Aux>
 );
+
 export default layout;
